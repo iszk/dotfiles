fe8d008adeffcf0a2a4400ac09f1e98f8fdb5417
yoh iszk <y@nemui.jp>
2024-08-09 13:56:43 +0900

    fix tool install

--- install.sh
+++ install.sh
@@ -2,8 +2,14 @@
 
 make sync
 
-if [ "$DEVCONTAINER" == "true" ]; then
+if [ "$REMOTE_CONTAINERS" == "true" ]; then
     echo "Running in a devcontainer. Executing devcontainer-specific commands."
-    echo "home = $HOME"
-    echo "user = $USER"
+    echo "EXTOOLS_SOURCE_DIR = $EXTOOLS_SOURCE_DIR"
+    echo "EXTOOLS_TARGET_DIR = $EXTOOLS_TARGET_DIR"
+    if [ -n "${EXTOOLS_SOURCE_DIR}" ]; then
+        /bin/bash $EXTOOLS_SOURCE_DIR/install.sh
+        if [ -n "${EXTOOLS_TARGET_DIR}" ]; then
+            ln -s $EXTOOLS_SOURCE_DIR $EXTOOLS_TARGET_DIR
+        fi
+    fi
 fi
