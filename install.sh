23d10c6c26104a803d68f794f4e4e888d51c4742
yoh iszk <y@nemui.jp>
2024-08-09 12:03:37 +0900

    fix for devcontainer

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 make sync
+
+if [ "$DEVCONTAINER" == "true" ]; then
+    echo "Running in a devcontainer. Executing devcontainer-specific commands."
+    echo "home = $HOME"
+    echo "user = $USER"
+fi
